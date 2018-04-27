3a49c90d2b336f3f40ecf8a1080ad169aeef78af
Shea McAuley <s179049@students.aie.edu.au>
2018-04-27 08:59:56 -0700

    Homework: Enemy AI and finished playerstats

---
+++ Assets/Scripts/InventoryUI.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryUI : MonoBehaviour {
+
+    public Transform itemsParent;
+    [SerializeField]
+    public GameObject inventoryUI;
+
+    InventorySlot[] slots;
+
+    Inventory inventory;
+
+	// Use this for initialization
+	void Start () {
+        inventory = Inventory.instance;
+        inventory.onItemChangedCallBack += UpdateUI;
+
+        slots = itemsParent.GetComponentsInChildren<InventorySlot>();
+
+	}
+
+    private void Update()
+    {
+        if(Input.GetButtonDown("Inventory"))
+        {
+            Debug.Log("Inventory button");
+            inventoryUI.SetActive(!inventoryUI.activeSelf);
+        }
+    }
+
+    void UpdateUI()
+    {
+        for (int i = 0; i < slots.Length; i++)
+        {
+            if(i < inventory.items.Count)
+            {
+                slots[i].AddItem(inventory.items[i]);
+            }
+            else
+            {
+                slots[i].ClearSlot();
+            }
+            
+        }
+
+    }
+}
